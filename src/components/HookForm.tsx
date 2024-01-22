569cad67d6f00731cc439e497b338e28b9f64277
Victor de Souza Magaldi <victmagaldi@hotmail.com>
2024-01-22 19:36:18 -0300

    react hook form

--- src/components/HookForm.tsx
+++ src/components/HookForm.tsx
@@ -1,32 +1,87 @@
-import { useForm, SubmitHandler } from "react-hook-form";
-
-type Inputs = {
-  example: string;
-  exampleRequired: string;
-};
+import { useEffect } from "react";
+import { set, useForm } from "react-hook-form";
 
 export function HookForm() {
   const {
     register,
     handleSubmit,
-    watch,
     formState: { errors },
-  } = useForm<Inputs>();
-  const onSubmit: SubmitHandler<Inputs> = (data) => console.log(data);
+    setValue,
+  } = useForm();
+  const onSubmit = (data: any) => console.log("data", data);
 
-  console.log(watch("example")); // watch input value by passing the name of it
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const req = await fetch("https://jsonplaceholder.typicode.com/todos/1");
+        const result: any = await req.json();
+
+        setValue("name", result.title);
+        setValue("email", result.title);
+        setValue("title", result.title);
+      } catch (error) {
+        console.error("Erro na requisição:", error);
+      }
+    };
+
+    fetchData();
+  }, []);
 
   return (
-    /* "handleSubmit" will validate your inputs before invoking "onSubmit" */
     <form onSubmit={handleSubmit(onSubmit)}>
-      {/* register your input into the hook by invoking the "register" function */}
-      <input defaultValue="test" {...register("example")} />
+      <label htmlFor="name">Name</label>
+      <input
+        id="name"
+        {...register("name", { required: true, maxLength: 30 })}
+      />
+      {errors.name && errors.name.type === "required" && (
+        <span>This is required</span>
+      )}
+      {errors.name && errors.name.type === "maxLength" && (
+        <span>Max length exceeded</span>
+      )}
 
-      {/* include validation with required or other standard HTML validation rules */}
-      <input {...register("exampleRequired", { required: true })} />
-      {/* errors will return when field validation fails  */}
-      {errors.exampleRequired && <span>This field is required</span>}
+      <label htmlFor="email">email</label>
+      <input
+        id="email"
+        {...register("email", { required: true, maxLength: 30 })}
+      />
+      {errors.email && errors.email.type === "required" && (
+        <span>This is required</span>
+      )}
+      {errors.email && errors.email.type === "maxLength" && (
+        <span>Max length exceeded</span>
+      )}
 
+      <label htmlFor="title">title</label>
+      <input
+        id="title"
+        {...register("title", { required: true, maxLength: 30 })}
+      />
+      {errors.title && errors.title.type === "required" && (
+        <span>This is required</span>
+      )}
+      {errors.title && errors.title.type === "maxLength" && (
+        <span>Max length exceeded</span>
+      )}
+
+      <label htmlFor="numero">numero</label>
+      <input
+        id="numero"
+        {...register("numero", {
+          required: true,
+          maxLength: 30,
+          onChange(evt) {
+            setValue("numero", evt.target.value.replace("5", ""));
+          },
+        })}
+      />
+      {errors.numero && errors.numero.type === "required" && (
+        <span>This is required</span>
+      )}
+      {errors.numero && errors.numero.type === "maxLength" && (
+        <span>Max length exceeded</span>
+      )}
       <input type="submit" />
     </form>
   );
