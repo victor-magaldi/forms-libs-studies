7b80287b37ee906078d12265a99c596e96386591
Victor de Souza Magaldi <victmagaldi@hotmail.com>
2024-01-26 19:15:35 -0300

    fix labels

--- src/components/Form/Inputs/CheckboxGroup.tsx
+++ src/components/Form/Inputs/CheckboxGroup.tsx
@@ -13,14 +13,15 @@ export const CheckboxGroup: React.FC<any> = (props) => {
   return (
     <div>
       {options.map((option: any) => (
-        <label key={option.value} htmlFor={props.name}>
+        <label key={option.value} htmlFor={option.value}>
           <input
             type="checkbox"
+            name={option.value}
             value={option.value}
+            id={option.value}
             onChange={(e) => {
               if (e.target.checked) {
                 onChange([...value, option.value]);
-                console.log("teste");
               } else {
                 onChange(value.filter((val: string) => val !== option.value));
               }
